899a61a95ca2baf0191d72c62c48510e73b34a82
Bogdan748 <andriesbogdan00@gmail.com>
2022-03-16 20:55:34 +0200

    re-throw exceptions

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -7,15 +7,36 @@ namespace Exceptions
     {
 		public static void Main()
 		{
+            try
+            {
+				ExampleWithExceptionFilters();
 
-			string[] list = StringArrayGenerator.RepeatString(10_000, "abc");
+			}
+			catch (ArgumentNullException argEx) when (ExceptionFilters.IsDirectoryOrFileException(argEx))
+			{
+				Console.WriteLine("Directory or file exception!");
+				Console.WriteLine(argEx.Message);
+				Console.WriteLine(argEx.StackTrace);
+			}
+			catch (ArgumentNullException argEx) when (ExceptionFilters.IsFileContentReplaceException(argEx))
+			{
+				Console.WriteLine("Content replace exception!");
+				Console.WriteLine(argEx.Message);
+				Console.WriteLine(argEx.StackTrace);
+			}
+            catch(FileProcessingException otherEx)
+            {
+				Console.WriteLine("An exception occured!");
+				Console.WriteLine(otherEx.Message);
+				Console.WriteLine(otherEx.StackTrace);
+				if (otherEx.InnerException is not null)
+                {
+					Console.WriteLine("Inner Exception");
+					Console.WriteLine(otherEx.InnerException.Message);
+					Console.WriteLine(otherEx.InnerException.StackTrace);
+				}
+			}
 
-			Stopwatch watch1 = Stopwatch.StartNew();
-			int[] integerArray1 = ConvertToIntArray_Normal(list);
-			watch1.Stop();
-			Console.WriteLine($"(1) List converted in {watch1.ElapsedMilliseconds} ms");
-			Console.WriteLine(integerArray1.Length);
-			Console.WriteLine(integerArray1[344]);
 		}
 
 		private static int[] ConvertToIntArray_Normal(string[] stringArray)
@@ -48,25 +69,32 @@ namespace Exceptions
 
 				FileProcessor.ReplaceText(directory, fileName, lookupText, replaceWithText);
 			}
-			catch (ArgumentNullException argEx) when (ExceptionFilters.IsDirectoryOrFileException(argEx))
-			{
-				Console.WriteLine("Directory or file exception!");
-				Console.WriteLine(argEx.Message);
-				Console.WriteLine(argEx.StackTrace);
-			}
-			catch (ArgumentNullException argEx) when (ExceptionFilters.IsFileContentReplaceException(argEx))
-			{
-				Console.WriteLine("Content replace exception!");
-				Console.WriteLine(argEx.Message);
-				Console.WriteLine(argEx.StackTrace);
-			}
+			
 			catch (Exception otherEx)
 			{
-				Console.WriteLine("Other exception occured!");
+				Console.WriteLine("An exception occured!");
 				Console.WriteLine(otherEx.Message);
 				Console.WriteLine(otherEx.StackTrace);
+
+
+				//throw;
+
+				throw new FileProcessingException("Something went wrong",otherEx);
+
 			}
 
+		}
+
+		private static void ExampleWithExceptionPerformance()
+        {
+			string[] list = StringArrayGenerator.RepeatString(10_000, "abc");
+
+			Stopwatch watch1 = Stopwatch.StartNew();
+			int[] integerArray1 = ConvertToIntArray_Normal(list);
+			watch1.Stop();
+			Console.WriteLine($"(1) List converted in {watch1.ElapsedMilliseconds} ms");
+			Console.WriteLine(integerArray1.Length);
+			Console.WriteLine(integerArray1[344]);
 		}
 	}
 }
